497a1ae9291a93557b9ef9eb4c594b17275d9910
Nikita <mst387@uky.edu>
2023-07-27 17:02:38 -0400

    Added file browser and editor

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-from microdot import send_file, Microdot
+from microdot import send_file, Microdot, Response
+import ujson
+
+settings = {}
+#read json using ujson 
+with open('settings.json') as f:
+    settings = ujson.load(f)
+
+# check if initial boot
+if settings['initial_boot']:
+    # do initial boot stuff
+    pass
+if not settings['initial_boot']:
+    # do normal boot stuff
+    pass
 
 app = Microdot()
 
@@ -6,16 +20,28 @@ app = Microdot()
 def index(request):
     return send_file('./pages/home.html')
 
-
 @app.route('/api/<path>')
 def api(request, path):
-    if path == 'settings':
-        return 'test'
+    if path == "editor":
+        return "/pages/editor.html"  # Return the URL to the HTML page instead of the file content
 
-@app.route('/assets/<path>')  
+@app.route('/assets/<path>')
 def assets(request, path):
     return send_file('./assets/' + path)
 
+@app.route('/pages/<path>')
+def pages(request, path):
+    return send_file('./pages/' + path)
+
+@app.route('ide/save', methods=['POST'])
+def save(request):
+    print(request.json)
+    name = request.json['name']
+    code = request.json['code']
+    # save code to file using name constant as name 
+    with open(name + '.py', 'w') as f:
+        f.write(code)
+    return Response('OK')
 
 
 app.run()
